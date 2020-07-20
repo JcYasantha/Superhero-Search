165563efbb93d24b8347a6224eff9ef2e0cbf942
JcYasantha <yasantha619@gmail.com>
2020-07-20 19:45:57 +0530

    UI updated with superhero details

--- script/index.js
+++ script/index.js
@@ -1,6 +1,12 @@
 const form = document.querySelector('.change-superhero');
+const card = document.querySelector('.card');
+const cardDetails = document.querySelector('.card-body');
+const superheroImage = document.querySelector('.superheroImage');
+const nodata = document.querySelector('.no-data');
 
-//get the superhero details
+const imageSrc = 'https://via.placeholder.com/480x100.png?text=Loading';
+
+//get the superhero details from the API
 const getSuperhero = async (superhero) => {
     const responses = await fetch(`https://superhero-search.p.rapidapi.com/?hero=${superhero}`, {
         "method": "GET",
@@ -12,13 +18,66 @@ const getSuperhero = async (superhero) => {
 
     const data = await responses.json();
     console.log(data);
+    return data;
 };
 
 form.addEventListener('submit', e => {
+    nodata.innerHTML = ``;
+    superheroImage.setAttribute('src', imageSrc);
     e.preventDefault();
 
     const superhero = form.superhero.value.trim();
     form.reset();
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+
+    getSuperhero(superhero)
+        .then(data => updateUI(data))
+        .catch(err => noData());
+
 });
 
-getSuperhero('deadpool');
+const noData = () => {
+    card.classList.add('d-none');
+    nodata.innerHTML = `<p>No Data Found</p>`;
+};
+
+const updateUI = (data) => {
+    superheroImage.setAttribute('src', data.images.sm);
+
+    cardDetails.innerHTML = `
+        <p><span class="font-weight-bold">Name : </span>${data.name}</p>
+        <p><span class="font-weight-bold">Full Name : </span>${data.biography.fullName}</p>
+        <p><span class="font-weight-bold">First Appearence : </span>${data.biography.firstAppearance}</p>
+        <p><span class="font-weight-bold">Publisher : </span>${data.biography.publisher}</p>
+        <div class="row">
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Combat : </span>${data.powerstats.combat}%</p>
+            </div>
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Durability : </span>${data.powerstats.durability}%</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Intelligence : </span>${data.powerstats.intelligence}%</p>
+            </div>
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Power : </span>${data.powerstats.power}%</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Speed : </span>${data.powerstats.speed}%</p>
+            </div>
+            <div class="col-sm">
+                <p><span class="font-weight-bold">Strength : </span>${data.powerstats.strength}%</p>
+            </div>
+        </div>
+        
+        
+        
+        `;
+};
