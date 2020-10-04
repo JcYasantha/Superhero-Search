f808c227601d86dbfe8651b65d28f6e0976ea00c
JcYasantha <yasantha619@gmail.com>
2020-10-04 22:16:45 +0530

    Card Updated

--- script/index.js
+++ script/index.js
@@ -1,53 +1,55 @@
-const form = document.querySelector('.change-superhero');
-const card = document.querySelector('.card');
-const cardDetails = document.querySelector('.card-body');
-const superheroImage = document.querySelector('.superheroImage');
-const nodata = document.querySelector('.no-data');
+const form = document.querySelector(".change-superhero");
+const card = document.querySelector(".card");
+const cardDetails = document.querySelector(".card-body");
+const superheroImage = document.querySelector(".superheroImage");
+const nodata = document.querySelector(".no-data");
 
-const imageSrc = 'https://via.placeholder.com/480x100.png?text=Loading';
+const imageSrc = "https://via.placeholder.com/480x100.png?text=Loading";
 
 //get the superhero details from the API
 const getSuperhero = async (superhero) => {
-    const responses = await fetch(`https://superhero-search.p.rapidapi.com/?hero=${superhero}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "superhero-search.p.rapidapi.com",
-            "x-rapidapi-key": "d81564b8c8msh978d801da9f622cp1b9a84jsn3aa3abcc2ced"
-        }
-    });
+  const responses = await fetch(
+    `https://superhero-search.p.rapidapi.com/?hero=${superhero}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "superhero-search.p.rapidapi.com",
+        "x-rapidapi-key": "d81564b8c8msh978d801da9f622cp1b9a84jsn3aa3abcc2ced",
+      },
+    }
+  );
 
-    const data = await responses.json();
-    console.log(data);
-    return data;
+  const data = await responses.json();
+  console.log(data);
+  return data;
 };
 
-form.addEventListener('submit', e => {
-    nodata.innerHTML = ``;
-    superheroImage.setAttribute('src', imageSrc);
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  nodata.innerHTML = ``;
+  superheroImage.setAttribute("src", imageSrc);
+  e.preventDefault();
 
-    const superhero = form.superhero.value.trim();
-    form.reset();
+  const superhero = form.superhero.value.trim();
+  form.reset();
 
-    if (card.classList.contains('d-none')) {
-        card.classList.remove('d-none');
-    }
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 
-    getSuperhero(superhero)
-        .then(data => updateUI(data))
-        .catch(err => noData());
-
+  getSuperhero(superhero)
+    .then((data) => updateUI(data))
+    .catch((err) => noData());
 });
 
 const noData = () => {
-    card.classList.add('d-none');
-    nodata.innerHTML = `<p>No Data Found</p>`;
+  card.classList.add("d-none");
+  nodata.innerHTML = `<p>No Data Found</p>`;
 };
 
 const updateUI = (data) => {
-    superheroImage.setAttribute('src', data.images.sm);
+  superheroImage.setAttribute("src", data.images.sm);
 
-    cardDetails.innerHTML = `
+  cardDetails.innerHTML = `
         <p><span class="font-weight-bold">Name : </span>${data.name}</p>
         <p><span class="font-weight-bold">Full Name : </span>${data.biography.fullName}</p>
         <p><span class="font-weight-bold">First Appearence : </span>${data.biography.firstAppearance}</p>
@@ -59,16 +61,14 @@ const updateUI = (data) => {
             <div class="col-sm">
                 <p><span class="font-weight-bold">Durability : </span>${data.powerstats.durability}%</p>
             </div>
-        </div>
-        <div class="row">
             <div class="col-sm">
                 <p><span class="font-weight-bold">Intelligence : </span>${data.powerstats.intelligence}%</p>
             </div>
+        </div>
+        <div class="row">  
             <div class="col-sm">
                 <p><span class="font-weight-bold">Power : </span>${data.powerstats.power}%</p>
             </div>
-        </div>
-        <div class="row">
             <div class="col-sm">
                 <p><span class="font-weight-bold">Speed : </span>${data.powerstats.speed}%</p>
             </div>
